6f3f84da6d168ea5835098eff4a3e09dc1771be6
Potrocio <john.feliciano4044@yahoo.com>
2022-11-15 06:35:48 -0500

    make working combination simulation without checkwinner function

--- logic.js
+++ logic.js
@@ -37,16 +37,16 @@ const objectConstructors = (() => {
 
 const constructedObjects = ((cell,player,pattern) => {
     const boardCells = (() => {
-        const cellOne = cell(1,'','cell one');
-        const cellTwo = cell(2,'','cell two');
-        const cellThree = cell(3,'','cell three');
-        const cellFour = cell(4,'','cell four');
-        const cellFive = cell(5,'','cell five');
-        const cellSix = cell(6,'','cell six');
-        const cellSeven = cell(7,'','cell seven');
-        const cellEight = cell(8,'','cell eight');
-        const cellNine = cell(9,'','cell nine');
-        return{cellOne,cellTwo,cellThree,cellFour,cellFive,cellSix,cellSeven,cellEight,cellNine};
+        const cell1 = cell(1,'','cell one');
+        const cell2 = cell(2,'','cell two');
+        const cell3 = cell(3,'','cell three');
+        const cell4 = cell(4,'','cell four');
+        const cell5 = cell(5,'','cell five');
+        const cell6 = cell(6,'','cell six');
+        const cell7 = cell(7,'','cell seven');
+        const cell8 = cell(8,'','cell eight');
+        const cell9 = cell(9,'','cell nine');
+        return{cell1,cell2,cell3,cell4,cell5,cell6,cell7,cell8,cell9};
     })()
 
     const players = (() => {
@@ -56,14 +56,14 @@ const constructedObjects = ((cell,player,pattern) => {
     })();
 
     const winningPatterns = (() => {
-        const rowOne = pattern(boardCells.cellOne,boardCells.cellTwo,boardCells.cellThree);
-        const rowTwo = pattern(boardCells.cellFour,boardCells.cellFive,boardCells.cellSix);
-        const rowThree = pattern(boardCells.cellSeven,boardCells.cellEight,boardCells.cellNine);
-        const columnOne = pattern(boardCells.cellOne,boardCells.cellFour,boardCells.cellSeven);
-        const columnTwo = pattern(boardCells.cellTwo,boardCells.cellFive,boardCells.cellEight);
-        const columnThree = pattern(boardCells.cellThree,boardCells.cellSix,boardCells.cellNine);
-        const rowOneDiagonal = pattern(boardCells.cellOne,boardCells.cellFive,boardCells.cellNine);
-        const rowThreeDiagonal = pattern(boardCells.cellThree,boardCells.cellFive,boardCells.cellSeven);
+        const rowOne = pattern(boardCells.cell1,boardCells.cell2,boardCells.cell3);
+        const rowTwo = pattern(boardCells.cell4,boardCells.cell5,boardCells.cell6);
+        const rowThree = pattern(boardCells.cell7,boardCells.cell8,boardCells.cell9);
+        const columnOne = pattern(boardCells.cell1,boardCells.cell4,boardCells.cell7);
+        const columnTwo = pattern(boardCells.cell2,boardCells.cell5,boardCells.cell8);
+        const columnThree = pattern(boardCells.cell3,boardCells.cell6,boardCells.cell9);
+        const rowOneDiagonal = pattern(boardCells.cell1,boardCells.cell5,boardCells.cell9);
+        const rowThreeDiagonal = pattern(boardCells.cell3,boardCells.cell5,boardCells.cell7);
         return{rowOne,rowTwo,rowThree,columnOne,columnTwo,columnThree,rowOneDiagonal,rowThreeDiagonal};
     })()
 
@@ -72,101 +72,177 @@ const constructedObjects = ((cell,player,pattern) => {
 
 //AI training
 
-let availableCells = ['cellOne','cellTwo','cellThree','cellFour','cellFive','cellSix','cellSeven','cellEight','cellNine'];
+let availableCells = ['cell1','cell2','cell3','cell4','cell5','cell6','cell7','cell8','cell9'];
 let combinationArray = [];
-for(let cell in availableCells) {
 
-    function resetBoardCells() {
-        for(let object in constructedObjects.boardCells) {
-            let objectToBeEdited = constructedObjects.boardCells[object];
-            objectToBeEdited.mark = '';
-        };    
-    }
-
-    resetBoardCells();
-    let combination = '000000000';
-    let currentPosition = constructedObjects.boardCells[availableCells[cell]].position;
-
-    constructedObjects.boardCells[availableCells[cell]].mark = 'X';
-    combination[0] = currentPosition;
-
-
-    let availableCells2 = availableCells.filter((item) => {
-        if(item !== availableCells[cell]) {
-            return true;
+function checkTrainingWinner() {
+    for (let object in constructedObjects.winningPatterns) {
+        object = constructedObjects.winningPatterns[object]
+        if(object.cellA.mark === object.cellB.mark && object.cellB.mark === object.cellC.mark) {
+            if(object.cellA.mark !== '') {
+                if(object.cellA.mark == 'X') {
+                    let status = 'lose';
+                    console.log('X wins')
+                    return status;
+                } else {
+                    let status = 'win';
+                    console.log('O wins')
+                    return status;
+                }; 
+            }
         }
-    })
+    };
+};
 
-    for(let cell in availableCells2) {
-        let currentPosition = constructedObjects.boardCells[availableCells2[cell]].position;
-        combination[1] = currentPosition;
-        constructedObjects.boardCells[availableCells2[cell]].mark = 'O';
-        let availableCells3 = availableCells2.filter((item) => {
-            if(item !== availableCells2[cell]) {
+function checkInterception(stage,marking,combinationIndex) {
+    for (let object in constructedObjects.winningPatterns) {
+        object = constructedObjects.winningPatterns[object]
+        let totalMarks = object.cellA.mark + object.cellB.mark + object.cellC.mark    
+        if(totalMarks.length == 2) {
+            for(let cell in object) {
+                if(object[cell].mark == '') {
+                    let objectPosition = 'cell' + object[cell].position;
+                    constructedObjects.boardCells[objectPosition].mark = marking;
+                    combination[combinationIndex] = object[cell].position;
+                    return combinationIndex;
+                }
+            }
+        } else {
+            combination[combinationIndex] = constructedObjects.boardCells[stage].position;
+            constructedObjects.boardCells[stage].mark = marking;
+            return combinationIndex;
+        }
+    }
+}
+
+
+loop1:
+    for(let cell in availableCells) {
+
+        function resetBoardCells() {
+            for(let object in constructedObjects.boardCells) {
+                let objectToBeEdited = constructedObjects.boardCells[object];
+                objectToBeEdited.mark = '';
+            };    
+        }
+
+        resetBoardCells();
+        var combination = [0,0,0,0,0,0,0,0,0];
+        // let currentPosition = constructedObjects.boardCells[availableCells[cell]].position;
+
+        // constructedObjects.boardCells[availableCells[cell]].mark = 'X';
+        checkInterception(availableCells[cell],'X',0)
+
+        // combination[0] = currentPosition;
+
+
+        let availableCells2 = availableCells.filter((item) => {
+            if(item !== availableCells[cell]) {
                 return true;
             }
         })
-        // console.log(combination,constructedObjects.boardCells)
+loop2:
+        for(let cell in availableCells2) {
+            let currentPosition = constructedObjects.boardCells[availableCells2[cell]].position;
+            // combination[1] = currentPosition;
+            // constructedObjects.boardCells[availableCells2[cell]].mark = 'O';
+            checkInterception(availableCells2[cell],'O',1)
 
-        for(let cell in availableCells3) {
-            let currentPosition = constructedObjects.boardCells[availableCells3[cell]].position;
-            combination[2] = currentPosition;
-            constructedObjects.boardCells[availableCells3[cell]].mark = 'X';
-            let availableCells4 = availableCells3.filter((item) => {
-                if(item !== availableCells3[cell]) {
+            let availableCells3 = availableCells2.filter((item) => {
+                if(item !== availableCells2[cell]) {
                     return true;
                 }
             })
-            for(let cell in availableCells4) {
-                let currentPosition = constructedObjects.boardCells[availableCells4[cell]].position;
-                combination[3] = currentPosition;
-                constructedObjects.boardCells[availableCells4[cell]].mark = 'O';
-                let availableCells5 = availableCells4.filter((item) => {
-                    if(item !== availableCells4[cell]) {
+            // console.log(combination,constructedObjects.boardCells)
+loop3:
+            for(let cell in availableCells3) {
+                let currentPosition = constructedObjects.boardCells[availableCells3[cell]].position;
+                // combination[2] = currentPosition;
+                // constructedObjects.boardCells[availableCells3[cell]].mark = 'X';
+                checkInterception(availableCells3[cell],'X',2)
+
+                let availableCells4 = availableCells3.filter((item) => {
+                    if(item !== availableCells3[cell]) {
                         return true;
                     }
                 })
-                for(let cell in availableCells5) {
-                    let currentPosition = constructedObjects.boardCells[availableCells5[cell]].position;
-                    combination[4] = currentPosition;
-                    constructedObjects.boardCells[availableCells5[cell]].mark = 'X';
-                    let availableCells6 = availableCells5.filter((item) => {
-                        if(item !== availableCells5[cell]) {
+loop4:
+                for(let cell in availableCells4) {
+                    let currentPosition = constructedObjects.boardCells[availableCells4[cell]].position;
+                    // combination[3] = currentPosition;
+                    // constructedObjects.boardCells[availableCells4[cell]].mark = 'O';
+                    checkInterception(availableCells4[cell],'O',3)
+                    let availableCells5 = availableCells4.filter((item) => {
+                        if(item !== availableCells4[cell]) {
                             return true;
                         }
                     })
-                    for(let cell in availableCells6) {
-                        let currentPosition = constructedObjects.boardCells[availableCells6[cell]].position;
-                        combination[5] = currentPosition;
-                        constructedObjects.boardCells[availableCells6[cell]].mark = 'O';
-                        let availableCells7 = availableCells6.filter((item) => {
-                            if(item !== availableCells6[cell]) {
+loop5:
+                    for(let cell in availableCells5) {
+                        let currentPosition = constructedObjects.boardCells[availableCells5[cell]].position;
+                        // combination[4] = currentPosition;
+                        // constructedObjects.boardCells[availableCells5[cell]].mark = 'X';
+                        checkInterception(availableCells5[cell],'X',4)
+                        let availableCells6 = availableCells5.filter((item) => {
+                            if(item !== availableCells5[cell]) {
                                 return true;
                             }
                         })
-                        for(let cell in availableCells7) {
-                            let currentPosition = constructedObjects.boardCells[availableCells7[cell]].position;
-                            combination[6] = currentPosition;
-                            constructedObjects.boardCells[availableCells7[cell]].mark = 'X';
-                            let availableCells8 = availableCells7.filter((item) => {
-                                if(item !== availableCells7[cell]) {
+loop6:
+                        for(let cell in availableCells6) {
+                            let currentPosition = constructedObjects.boardCells[availableCells6[cell]].position;
+                            // combination[5] = currentPosition;
+                            // constructedObjects.boardCells[availableCells6[cell]].mark = 'O';
+                            checkInterception(availableCells6[cell],'O',5)
+
+                            let availableCells7 = availableCells6.filter((item) => {
+                                if(item !== availableCells6[cell]) {
                                     return true;
                                 }
                             })
-                            for(let cell in availableCells8) {
-                                let currentPosition = constructedObjects.boardCells[availableCells8[cell]].position;
-                                combination[7] = currentPosition;
-                                constructedObjects.boardCells[availableCells8[cell]].mark = 'O';
-                                let availableCells9 = availableCells8.filter((item) => {
-                                    if(item !== availableCells8[cell]) {
+loop7:
+                            for(let cell in availableCells7) {
+                                let currentPosition = constructedObjects.boardCells[availableCells7[cell]].position;
+                                // combination[6] = currentPosition;
+                                // constructedObjects.boardCells[availableCells7[cell]].mark = 'X';
+                                checkInterception(availableCells7[cell],'X',6)
+
+                                let availableCells8 = availableCells7.filter((item) => {
+                                    if(item !== availableCells7[cell]) {
                                         return true;
                                     }
                                 })
-                                for(let cell in availableCells9) {
-                                    let currentPosition = constructedObjects.boardCells[availableCells9[cell]].position;
-                                    combination[8] = currentPosition;
-                                    constructedObjects.boardCells[availableCells9[cell]].mark = 'X';
-                                    combinationArray.push(combination);
+loop8:
+                                for(let cell in availableCells8) {
+                                    let currentPosition = constructedObjects.boardCells[availableCells8[cell]].position;
+                                    // combination[7] = currentPosition;
+                                    // constructedObjects.boardCells[availableCells8[cell]].mark = 'O';
+                                    checkInterception(availableCells8[cell],'O',7)
+
+                                    let availableCells9 = availableCells8.filter((item) => {
+                                        if(item !== availableCells8[cell]) {
+                                            return true;
+                                        }
+                                    })
+loop9:
+                                    for(let cell in availableCells9) {
+                                        let currentPosition = constructedObjects.boardCells[availableCells9[cell]].position;
+                                        // combination[8] = currentPosition;
+                                        // constructedObjects.boardCells[availableCells9[cell]].mark = 'X';
+                                        checkInterception(availableCells9[cell],'X',8)
+
+                                        let finalCombination = ''
+                                        let finalBoardCells = ''
+                                        for(let i=0 ; i < combination.length; i++) {
+                                            finalCombination += combination[i]
+                                            finalBoardCells += constructedObjects.boardCells['cell'+ (i+1)].mark;
+                                        }
+                                        console.log(finalBoardCells,finalCombination)
+                                        combinationArray.push([finalCombination]);
+                                        if(combinationArray.length == 100) {
+                                            break loop1;
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -174,9 +250,9 @@ for(let cell in availableCells) {
                 }
             }
         }
+        
     }
-    
-}
+
 
 //game events
 const gameEvents = (() => {
